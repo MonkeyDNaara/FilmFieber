f7e3b6170aa9a31fddec77848bcb67c2b7b999fc
chr-d <275697199+chr-d@users.noreply.github.com>
2026-07-29 19:08:07 +0200

    add config for multiple html files, add links for testing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        journal: resolve(__dirname, "journal.html"),
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
