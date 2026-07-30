cb076bdb0e84106b0c6463dba9823c1ab5a8f202
Niko <naara21@hotmail.de>
2026-07-30 15:59:25 +0200

    reading DOM-elements in main.js and fournal.js

---
+++ src/journal.js
@@ -0,0 +1,8 @@
+// Imports
+
+// DOM-Elemente auslesen
+const cardContainer = document.querySelector("#card_container");
+const form = document.querySelector("#search_form");
+
+// Export
+export { cardContainer, form };
